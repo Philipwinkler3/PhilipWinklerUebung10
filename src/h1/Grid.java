197912460b6ec2ea2dbe97c08e866b9f597ddacd
philip winkler <pwinkler@smail.uni-koeln.de>
2026-01-12 13:07:30 +0100

    hausaufgaben

--- src/h1/Grid.java
+++ src/h1/Grid.java
@@ -2,4 +2,64 @@ package h1;
 
 public class Grid {
 
+    private Cell[][] gridArray;
+
+    public Cell[][] getGridArray() {
+        return gridArray;
+    }
+
+    public void setGridArray(Cell[][] gridArray) {
+        this.gridArray = gridArray;
+    }
+
+    public Grid(Cell[] cells, int gridRows, int gridCols) {
+        gridArray = new Cell[gridRows][gridCols];
+
+        for (int r = 0; r < gridRows; r++) {
+            for (int c = 0; c < gridCols; c++) {
+                gridArray[r][c] = new Cell(r, c, false);
+            }
+        }
+
+        if (cells != null) {
+            for (Cell cell : cells) {
+                if (cell == null) continue;
+
+                int r = cell.getIndexRow();
+                int c = cell.getIndexCol();
+
+                if (r >= 0 && r < gridRows && c >= 0 && c < gridCols) {
+                    gridArray[r][c].setAlive(true);
+                }
+            }
+        }
+
+        countLivingNeighbors();
+    }
+
+    private void countLivingNeighbors() {
+        for (int r = 0; r < gridArray.length; r++) {
+            for (int c = 0; c < gridArray[0].length; c++) {
+                gridArray[r][c].countLivingNeighbors(gridArray);
+            }
+        }
+    }
+
+    public void computeNextGen() {
+        countLivingNeighbors();
+
+        for (int r = 0; r < gridArray.length; r++) {
+            for (int c = 0; c < gridArray[0].length; c++) {
+                gridArray[r][c].setAlive(gridArray[r][c].isAliveNextGen());
+            }
+        }
+
+        countLivingNeighbors();
+    }
+
+    public void computeGeneration(int n) {
+        for (int i = 0; i < n; i++) {
+            computeNextGen();
+        }
+    }
 }
--- src/h2/Spielstein.java
+++ src/h2/Spielstein.java
@@ -1,5 +1,76 @@
 package h2;
 
 public class Spielstein {
+    private int currentRow;
+    private int currentCol;
+    private Spielbrett brett;
 
+    public Spielstein(Spielbrett brett, int indexRow, int indexCol) {
+        this.brett = brett;
+        this.currentRow = indexRow;
+        this.currentCol = indexCol;
+    }
+
+    public int getCurrentRow() {
+        return currentRow;
+    }
+
+    public void setCurrentRow(int currentRow) {
+        this.currentRow = currentRow;
+    }
+
+    public int getCurrentCol() {
+        return currentCol;
+    }
+
+    public void setCurrentCol(int currentCol) {
+        this.currentCol = currentCol;
+    }
+
+    public Spielbrett getBrett() {
+        return brett;
+    }
+
+    public void setBrett(Spielbrett brett) {
+        this.brett = brett;
+    }
+
+    private boolean movesOut() {
+        Feld[][] b = brett.getBrett();
+        int dim = brett.getDim();
+
+        Feld current = b[currentRow][currentCol];
+        char dir = current.getDirection();
+
+        if (dir == 'U') return currentRow - 1 < 0;
+        if (dir == 'D') return currentRow + 1 >= dim;
+        if (dir == 'L') return currentCol - 1 < 0;
+        if (dir == 'R') return currentCol + 1 >= dim;
+
+        return false;
+    }
+
+    public void go(int n) {
+        Feld[][] b = brett.getBrett();
+        int dim = brett.getDim();
+
+        for (int i = 0; i < n; i++) {
+            Feld current = b[currentRow][currentCol];
+
+            if (current.isBoese()) {
+                continue;
+            }
+
+            if (movesOut()) {
+                continue;
+            }
+
+            char dir = current.getDirection();
+
+            if (dir == 'U') currentRow--;
+            else if (dir == 'D') currentRow++;
+            else if (dir == 'L') currentCol--;
+            else if (dir == 'R') currentCol++;
+        }
+    }
 }
